55cad44e03fe156a94d30c261a55e53c475e2d7c
howard.zuo <leftstick@qq.com>
2015-08-22 23:27:29 +0800

    finish first part of document

--- legend.js
+++ legend.js
@@ -1,6 +1,36 @@
 (function(win, $) {
     'use strict';
 
+    var $authors = $('.authors');
 
+    $('#expand').on('click', function(e) {
+        $(e.target).toggleClass('icon-ellipsis').toggleClass('icon-resize-small');
+        $authors.toggleClass('show');
+        return false;
+    });
+
+    $('.show-answer').on('click', function() {
+        $(this).toggleClass('teal').toggleClass('blue').siblings('.answer').toggleClass('show');
+        return false;
+    });
+
+    $('.icon-success').hide();
+    $('.icon-close').hide();
+
+    $('input[type="radio"]').on('change', function() {
+        var $this = $(this);
+        var group = $this.attr('name');
+        var isCorrect = typeof $this.attr('correct') !== 'undefined';
+        if (isCorrect) {
+            $this.siblings('.icon-success').show();
+            $this.siblings('.icon-close').hide();
+        } else {
+            $this.siblings('.icon-success').hide();
+            $this.siblings('.icon-close').show();
+        }
+        return false;
+    });
+
+    $('.modal-trigger').leanModal();
 
 }(this, jQuery));
